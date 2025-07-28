865976a18554d90c34b9c14e3cca4a270ffde696
Alaa <alaa.ashraf.atef@gmail.com>
2025-07-28 13:41:28 +0300

    added patch services and router

--- app/routers/vlans.py
+++ app/routers/vlans.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.params import Body
 from app.services.Delete_Vlans_Services import delete_all_vlans_from_switch, delete_vlan_by_name, delete_vlan_description_by_name
 from app.services.Get_Vlans_Services import fetch_vlans
 from app.models.vlan import Vlan_request
+from app.services.Patch_Vlans_Services import update_vlans
 from app.services.Put_Vlans_Services import update_put_vlan
 from app.services.Post_Vlans_Services import add_vlans
+
+
 router = APIRouter()
-
-
 
 @router.get("/")
 async def get_vlans():
@@ -52,3 +54,7 @@ async def delete_single_vlan(vlan_name: str):
 )
 async def delete_vlan_description(vlan_name: str):
     return await delete_vlan_description_by_name(vlan_name)    
+
+@router.patch("/patch")
+async def patch_vlans(vlan_data: dict = Body(...)): 
+    return await update_vlans(vlan_data)
---
+++ app/services/Patch_Vlans_Services.py
@@ -0,0 +1,83 @@
+from fastapi import HTTPException
+import httpx
+import os
+from dotenv import load_dotenv
+from email.utils import formatdate
+from datetime import datetime, timezone
+
+
+load_dotenv()
+ 
+SONIC_BASE_URL = os.getenv("SONIC_BASE_URL")
+
+RESTCONF_HEADERS = {
+    "Accept": "application/yang-data+json",
+    "Content-Type": "application/yang-data+json",
+}
+
+ETH_INTERFACES = {"Ethernet0", "Ethernet1", "Ethernet2", "Ethernet3" , "Ethernet4" , "Ethernet5" , "Ethernet6"}  # till get_ethernet_interfaces is made 
+
+
+async def update_vlans(vlan_data: dict):
+    
+    validate_vlan_data(vlan_data)
+    try:
+        async with httpx.AsyncClient(verify=False, timeout=10.0) as client:
+            response = await client.patch(
+                f"{SONIC_BASE_URL}/restconf/data/sonic-vlan:sonic-vlan",
+                headers=RESTCONF_HEADERS,
+                json=vlan_data,
+            )
+            response.raise_for_status()
+
+            
+            if response.text.strip():
+                return response.json()
+            
+            
+            return {
+                "message": "VLAN configuration updated successfully.",
+                "status": response.status_code,
+                "date": formatdate(timeval=None, usegmt=True)
+            }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+    
+
+def validate_vlan_data(vlan_data: dict):
+    try:
+        vlan_config = vlan_data.get("sonic-vlan:sonic-vlan", {})
+        vlan_list = vlan_config.get("VLAN", {}).get("VLAN_LIST", [])
+        member_list = vlan_config.get("VLAN_MEMBER", {}).get("VLAN_MEMBER_LIST", [])
+
+        for vlan in vlan_list:
+            vlanid = vlan.get("vlanid")
+            name = vlan.get("name")
+            mac_learning = vlan.get("mac_learning")
+
+            if mac_learning not in {"enabled", "disabled"}:
+                raise ValueError(f"Invalid mac_learning '{mac_learning}'. Must be 'enabled' or 'disabled'.")
+
+            
+            if name != f"Vlan{vlanid}":
+                raise ValueError(f"VLAN name must match 'Vlan<id>' pattern (got '{name}', expected 'Vlan{vlanid}')")
+
+            
+            if not (1 <= vlanid <= 4094):
+                raise ValueError(f"VLAN ID must be between 1 and 4094 (got {vlanid})")
+
+        for member in member_list:
+            ifname = member.get("ifname")
+            if ifname not in ETH_INTERFACES:
+                raise ValueError(f"Invalid interface name '{ifname}'. Allowed: {', '.join(ETH_INTERFACES)}")
+
+            if member.get("tagging_mode") not in {"tagged", "untagged"}:
+                raise ValueError(f"Invalid tagging_mode '{member.get('tagging_mode')}'. Must be 'tagged' or 'untagged'.")
+
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+
+
