596dea85201626aa36ba86b05eebd4ed433a50c2
Alaa <alaa.ashraf.atef@gmail.com>
2025-07-29 11:28:18 +0300

    updated req.txt, removed auth part in delete Vlans

--- app/services/Delete_Vlans_Services.py
+++ app/services/Delete_Vlans_Services.py
@@ -12,12 +12,11 @@ load_dotenv()
 SONIC_SWITCH_IP=os.getenv("SONIC_SWITCH_IP")
 SONIC_BASE_URL=os.getenv("SONIC_BASE_URL")
 
-AUTH = ("admin", "sonic") 
 
 async def delete_all_vlans_from_switch():
     try:
         async with httpx.AsyncClient(verify=False) as client:
-            response = await client.delete(f"{SONIC_BASE_URL}/restconf/data/sonic-vlan:sonic-vlan", headers=RESTCONF_HEADERS, auth=AUTH, timeout=5.0)
+            response = await client.delete(f"{SONIC_BASE_URL}/restconf/data/sonic-vlan:sonic-vlan", headers=RESTCONF_HEADERS, timeout=10.0)
 
             response.raise_for_status()
 
@@ -34,7 +33,7 @@ async def delete_vlan_by_name(vlan_name: str):
     vlan_url = f"{SONIC_BASE_URL}/restconf/data/sonic-vlan:sonic-vlan/VLAN/VLAN_LIST={vlan_name}"
     try:
         async with httpx.AsyncClient(verify=False) as client:
-            response = await client.delete(vlan_url, headers=RESTCONF_HEADERS, auth=AUTH, timeout=5.0)
+            response = await client.delete(vlan_url, headers=RESTCONF_HEADERS,  timeout=10.0)
 
             response.raise_for_status()
             return {"detail": f"VLAN '{vlan_name}' successfully deleted from the switch."}
@@ -50,7 +49,7 @@ async def delete_vlan_description_by_name(vlan_name: str):
     url = f"{SONIC_BASE_URL}/restconf/data/sonic-vlan:sonic-vlan/VLAN/VLAN_LIST={vlan_name}/description"
     try:
         async with httpx.AsyncClient(verify=False) as client:
-            response = await client.delete(url, headers=RESTCONF_HEADERS, auth=AUTH, timeout=5.0)
+            response = await client.delete(url, headers=RESTCONF_HEADERS,  timeout=10.0)
 
             response.raise_for_status()
             return {"detail": f"VLAN '{vlan_name}' Description successfully deleted from the switch."}
