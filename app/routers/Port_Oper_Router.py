683f8fbab897d03f7c43174c3c41bdc3b8675e81
ranatamerr <roneytamer@gmail.com>
2025-08-06 15:35:32 +0300

    Add port status summary endpoint

--- app/routers/Port_Oper_Router.py
+++ app/routers/Port_Oper_Router.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import APIRouter
-from app.services.Get_Port_Op_Services import get_po_service
+from app.models.HomePortSummary import PortSummaryList
+from app.services.Port_Op_Services import get_po_service, get_port_summary_service
 
 
 router = APIRouter()
@@ -10,3 +11,7 @@ router = APIRouter()
 @router.get("/")
 async def get_port_oper():
     return await get_po_service()
+
+@router.get("/status-summary", response_model=PortSummaryList)
+async def get_port_summary():
+    return await get_port_summary_service()
