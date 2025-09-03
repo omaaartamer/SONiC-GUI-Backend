4a7d7e5f16a93f57846f351f127f09d129a8a2d2
ranatamerr <roneytamer@gmail.com>
2025-09-03 12:27:26 +0300

    added temperature parser

--- app/test_fan_psu_parser.py
+++ app/test_fan_psu_parser.py
@@ -1,9 +1,10 @@
 import pprint
-from services.SSH_Services import parse_fan_output, parse_psu_output
+from services.SSH_Services import parse_fan_output, parse_psu_output, parse_temperature_output
 
 # Sample Fan CLI output
 cli_output_fans = """
 Drawer    LED    FAN      Speed   Direction  Presence  Status  Timestamp
+----------------------------------------------------------------------------------
 FanTray1  green  FAN-1F   40%     exhaust    Present   OK      20250815 14:29:02
 FanTray1  green  FAN-1R   40%     exhaust    Present   OK      20250815 14:29:02
 FanTray2  green  FAN-2F   40%     exhaust    Present   OK      20250815 14:29:02
@@ -13,9 +14,19 @@ N/A       green  PSU-1 FAN-1 14% exhaust    Present   OK      20250815 14:29:03
 # Sample PSU CLI output
 cli_output_psus = """
 PSU    Model    Serial               HW Rev  Voltage (V)  Current (A)  Power (W)  Status    LED
+----------------------------------------------------------------------------------------------------
 PSU 1  YM-1401A SA080V062146002274   N/A     11.74        6.34         79.00      OK        green
 PSU 2  YM-1401A SA080V062146002222   N/A     0.00         0.00         0.00       NOT OK    red
 """
+# Sample Temperature CLI output
+cli_output_temperature = """
+Sensor       Location       Temperature (C)    Threshold (C)     Status      Timestamp
+------------------------------------------------------------------------------------
+Temp Sensor 1  CPU            45                 75                OK          2023-10-27 10:30:00
+Temp Sensor 2  GPU            55                 85                WARNING     2023-10-27 10:30:01
+Temp Sensor 3  Mainboard      38                 60                OK          2023-10-27 10:30:02
+"""
+
 
 # Test Fan Parser
 parsed_fans = parse_fan_output(cli_output_fans)
@@ -28,3 +39,7 @@ print("\n----------------------------------\n")
 parsed_psus = parse_psu_output(cli_output_psus)
 print("🔌 Parsed PSUs:")
 pprint.pprint(parsed_psus)
+
+# Test Temperature Parser
+print("🌡️ Parsed Temperatures:")
+pprint.pprint(parse_temperature_output(cli_output_temperature))
